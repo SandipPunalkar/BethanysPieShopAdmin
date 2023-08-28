4577b211a9218c216c254c84c435bfeebcab4a96
Sandip Punalkar <sandippunalkar9@gmail.com>
2023-08-28 12:17:00 +0530

    Add seed data.

---
+++ BethanysPieShopAdmin/Data/Configuration/CategoryEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShopAdmin.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShopAdmin.Data.Configuration
+{
+    public class CategoryEntityTypeConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder
+                .Property(b => b.Name)
+                .IsRequired();
+        }
+    }
+}
